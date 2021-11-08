81b8c2251cdc364e1c8bcc89ee4654e85064a099
denev <denev723@gmail.com>
2021-11-08 15:22:36 +0900

    search menu 작업

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
-  $("a[href='javascript:void(0)']").each((index, item) =>
-    item.addEventListener("click", () => alert("준비중입니다.")),
-  );
+  var links = $("a[href='javascript:void(0)']");
+  $.each(links, function (index, item) {
+    item.addEventListener("click", function () {
+      alert("준비중입니다.");
+    });
+  });
+
+  var mql = window.matchMedia("screen and (-webkit-min-device-pixel-ratio:0)");
+  if (mql.matches) {
+    console.log("hey!!");
+  } else {
+    console.log("hoo!");
+  }
 });
